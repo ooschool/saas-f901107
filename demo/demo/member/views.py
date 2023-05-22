e54e79386630ecfdb4f5de811dcfd8c82ca7d0ce
Cheng-Lin Tsai <f901107@hotmail.com>
2023-05-22 14:08:10 +0800

    Add files via upload

---
+++ demo/demo/member/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def show_member_info(request):
+    return render(request, 'member_page.html',{
+        'member_name': 'Devoe',
+        'member_age':'23'
+    })
